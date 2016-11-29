68c6c1684d35c9c866456abc06e12445e5f4e96f
chinawanxd <wanxiaodong163@163.com>
2016-11-29 13:59:51 +0800

    git

--- spboot_02/src/main/java/wan/excute/ExcuteBean.java
+++ spboot_02/src/main/java/wan/excute/ExcuteBean.java
@@ -10,7 +10,7 @@ public class ExcuteBean {
 
 	/**
 	 * @param args
-	 */
+	 */  
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext context=
         	new AnnotationConfigApplicationContext(JavaConfig.class);
